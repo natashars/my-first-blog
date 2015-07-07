8ad90adc410c90bb08d837ace50ead52571fd17b
natasha <natasha.rs2@fhs-nw1.org.uk>
2015-07-07 15:40:35 +0100

    added views to create/edit blog post inside the site

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
 
     def publish(self):
         self.published_date=timezone.now()
-        slef.save()
+        self.save()
 
-    def _str_(self):
+    def __str__(self):
         return self.title
